fe8fc31e0190cdc37f60eae2c61782164b31ea03
Benjamin Carson <benjohncarson@gmail.com>
2017-01-26 17:05:00 +0000

    22 prevent duplicate registrations

--- app/app.rb
+++ app/app.rb
@@ -34,7 +34,7 @@ class BookmarkManager < Sinatra::Base
             session[:user_id] = @user.id
             redirect '/'
         else
-            flash.now[:notice] = "Password and confirmation password do not match"
+            flash.now[:errors] = @user.errors.full_messages
             erb :'users/new'
         end
     end
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -3,19 +3,27 @@ feature "user signup" do
     scenario 'requires a matching confirmation password' do
         expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
         expect(current_path).to eq('/users')
-        expect(page).to have_content "Password and confirmation password do not match"
+        expect(page).to have_content "Password does not match the confirmation"
     end
     
     scenario "I can't sign up without an email address" do
         expect { sign_up(email:nil) }.not_to change(User, :count)
+        expect(page).to have_content('Email must not be blank')
     end
     
     scenario "I can't sign up with an invalid email address" do
         expect {sign_up(email: "invalid@email") }.not_to change(User, :count)
+        expect(current_path).to eq('/users')
+        expect(page).to have_content('Email has an invalid format')
     end
     
+    scenario "I can't sign up with an email address that already exists" do
+       sign_up
+       expect { sign_up }.not_to change(User, :count)
+       expect(page).to have_content('Email is already taken')
+    end
+   
     scenario "can sign up to the app" do
-        sign_up
         expect { sign_up }.to change(User, :count).by(1)
         expect(current_path).to eq '/links'
         expect(page).to have_content('Welcome, michael@jackson.com')
