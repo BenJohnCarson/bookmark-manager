59bab21dd43fc62d6350641b87e29afe6fed7ad6
Benjamin Carson <benjohncarson@gmail.com>
2017-01-26 14:08:56 +0000

    ex19 password confirmation

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,4 +1,9 @@
 feature "user signup" do
+    
+    scenario 'requires a matching confirmation password' do
+        expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    end
+    
     scenario "can sign up to the app" do
         sign_up
         expect { sign_up }.to change(User, :count).by(1)
